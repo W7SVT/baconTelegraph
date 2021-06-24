71f0487fb96569b5c148cce5f1a031558379178c
W7SVT <85949017+W7SVT@users.noreply.github.com>
2021-06-24 12:43:35 -0700

    updated to case

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #########################################################
-# Created by W7SVT May 2021 #############################
+# Created by W7SVT May 2021  ############################
+# Updated by W7SVT June 2021 ############################
 #########################################################
 #########################################################
 #  __      ___________  _____________   _______________ #
@@ -10,30 +11,59 @@
 #   \__/\  /   /____/ /_______  /   \___/     |____|    #
 #        \/                   \/                        #
 #########################################################
-sudo sed -i "s/# en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
-sudo locale-gen
 
-FILE=$(zenity --file-selection --title="Select a html file" --file-filter="*.html")
-if [ -z  "$FILE" ]
-then
-    exit 1
-fi
+ask=$(zenity --title "baconTelegraph Install" \
+	--text "Estimate Speed" \
+	--list \
+	--radiolist \
+	--width=400 \
+	--height=400 \
+	--multiple \
+	--column "Install" --column "Tool" \
+    False "Prerequisites only" \
+    False "hamlib" \
+    False "JTDX" \
+    False "WSJT-X" \
+    False "CHIRP" \
+    False "Conky" \
+    False "JS8call" \
+    )
+echo $ask
 
-if zenity --question --title="HTML viewer" --text="Would you like to open this file with Browser?"
-then
-    xdg-open "file://${FILE}"
-else
-    zenity --text-info --filename="$FILE" --html --title="HTML viewer"
-fi
-
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y \
-  curl \
-  cmake \
-  pavucontrol \
-  git \
-  python3 \
-  python3-pip
-sudo pip3 install pi-ina219
-
-pcmanfm --set-wallpaper $HOME/baconTelegraph/files/radioRoom2.jpeg 
+case $ask in
+   "Prerequisites only") \
+     sudo apt update && sudo apt upgrade -y
+     sudo apt install -y \
+        curl \
+        cmake \
+        pavucontrol \
+        git \
+        python3 \
+        python3-pip
+    sudo pip3 install pi-ina219
+    sudo sed -i "s/# en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+    sudo locale-gen
+    
+    pcmanfm --set-wallpaper $HOME/baconTelegraph/files/radioRoom2.jpeg 
+;;
+   "hamlib") \
+/bin/sh $HOME/baconTelegraph/hamlib.sh
+;;
+   "JTDX") \
+/bin/sh $HOME/baconTelegraph/jtdx.sh
+;;
+   "WSJT-X") \
+/bin/sh $HOME/baconTelegraph/wsjtx.sh
+;;
+   "CHIRP") \
+/bin/sh $HOME/baconTelegraph/chirp.sh
+;;
+   "Conky") \
+/bin/sh $HOME/baconTelegraph/conky.sh
+;;
+   "JS8call") \
+/bin/sh $HOME/baconTelegraph/js8call.sh
+;;
+   *) echo "Sorry, no selection made";;
+esac
+/bin/sh test.sh
