58761184a2c59b6ad1948c129e39461c711d9d70
Drew S <drew@zapatatechnology.com>
2021-06-24 11:42:56 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#########################################################
+# Created by W7SVT May 2021 #############################
+#########################################################
+#########################################################
+#  __      ___________  _____________   _______________ #
+# /  \    /  \______  \/   _____/\   \ /   /\__    ___/ #
+# \   \/\/   /   /    /\_____  \  \   Y   /   |    |    #
+#  \        /   /    / /        \  \     /    |    |    #
+#   \__/\  /   /____/ /_______  /   \___/     |____|    #
+#        \/                   \/                        #
+#########################################################
+sudo sed -i "s/# en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+sudo locale-gen
+
+FILE=$(zenity --file-selection --title="Select a html file" --file-filter="*.html")
+if [ -z  "$FILE" ]
+then
+    exit 1
+fi
+
+if zenity --question --title="HTML viewer" --text="Would you like to open this file with Browser?"
+then
+    xdg-open "file://${FILE}"
+else
+    zenity --text-info --filename="$FILE" --html --title="HTML viewer"
+fi
