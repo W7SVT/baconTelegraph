c0edc3f18aba830414f34db41406ed61297bbb62
W7SVT <w7svt@icloud.com>
2022-05-10 18:43:14 -0700

    Update to whiptail UI

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #########################################################
 # Created by W7SVT APR 2022  ############################
 # Updated by W7SVT APR 2022  ############################
@@ -11,12 +11,10 @@
 #   \__/\  /   /____/ /_______  /   \___/     |____|    #
 #        \/                   \/                        #
 #########################################################
-source ~/.bashrc
 
-echo "***************************************************"
-echo "# Read/Set CALLSIGN                               #"
-echo "***************************************************"
-sleep 1
+#**************************************************
+#  Read/Set CALLSIGN                              #
+#**************************************************
 
 [[ x"${CALLSIGN}" == "x" ]] \
 && CALLSIGN=$(whiptail --title "Set CALLSIGN" \
@@ -24,12 +22,11 @@ sleep 1
 && export CALLSIGN \
 && sudo echo "export CALLSIGN=$CALLSIGN" >> $HOME/.bashrc \
 && source ~/.bashrc \
-|| echo "Your CALLSIGN is set to '$CALLSIGN'"
+|| sleep 1 
 
-echo "***************************************************"
-echo "# Read/Set GRID                                   #"
-echo "***************************************************"
-sleep 1
+#**************************************************
+# Read/Set GRID                                   #
+#**************************************************
 
 [[ x"${GRID}" == "x" ]] \
 && GRID=$(whiptail --title "Set GRID" \
@@ -37,36 +34,32 @@ sleep 1
 && export GRID \
 && sudo echo "export GRID=$GRID" >> $HOME/.bashrc \
 && source ~/.bashrc \
-|| echo "Your GRID is set to '$GRID'"
+|| sleep 1
 
+# Display if GRID and CALLSIGN are set
 
+TERM=ansi  whiptail --title "Howdy , Welcome to baconTelegraph" --infobox "Your CALL is set to ${CALLSIGN} and are in maidenhead grid ${GRID}" 8 78 
+sleep 4
 
 echo "***************************************************"
-echo "# Zenity Install Menu#"
+echo "# whiptail Install Menu#"
 echo "***************************************************"
 
-ask=$(zenity --title "baconTelegraph Install" \
-	--text "Proof that Pi is irrational" \
-	--list \
-	--radiolist \
-	--width=400 \
-	--height=400 \
-	--multiple \
-	--column "Install" --column "Tool" \
-    False "Prerequisites only" \
-    False "hamlib" \
-    False "JTDX From SRC" \
-    False "WSJT-X From SRC" \
-    False "JS8call From SRC" \
-    False "CHIRP" \
-    False "Conky" \
-    False "Conky Small Screen" \
 
-    )
-echo $ask
+SELECTED=$(whiptail --separate-output --backtitle "Proof that Pi is irrational" \
+   --title "SELECT PACKAGES TO INSTALL" --radiolist \
+   "${callsign} Welcome to baconTelegraph" 20 100 10 \
+   Prerequisites "Install only prerequisites and none of the apps" ON \
+   hamlib "Rig control for most radios" OFF \
+   JTDX "Feature Rich Software for FT8 and Other JT Modes" OFF \
+   WSJT-X "FST4(W), FT4, FT8, JT4, JT9, JT65, Q65, MSK144, & WSPR" OFF \
+   JS8Call "Digital weak signal keyboard to keyboard messaging" OFF \
+   CHIRP "Open Source Radio Programmer" OFF \
+   Conky "On Screen Display" OFF \
+   ConkySM "On Screen Display - Small Screen Version" OFF 3>&1 1>&2 2>&3)
 
-case $ask in
-   "Prerequisites only") \
+case $SELECTED in
+   "Prerequisites") \
      sudo apt update && sudo apt upgrade -y
      sudo apt install -y \
         curl \
@@ -90,13 +83,10 @@ case $ask in
    "hamlib") \
 /bin/sh $HOME/baconTelegraph/hamlib.sh
 ;;
-   "JTDX From SRC") \
+   "JTDX") \
 /bin/sh $HOME/baconTelegraph/jtdx_src.sh
 ;;
    "WSJT-X") \
-/bin/sh $HOME/baconTelegraph/wsjtx.sh
-;;
-   "WSJT-X From SRC") \
 /bin/sh $HOME/baconTelegraph/wsjtx_src.sh
 ;;
    "CHIRP") \
@@ -105,10 +95,10 @@ case $ask in
    "Conky") \
 /bin/sh $HOME/baconTelegraph/conky.sh
 ;;
-   "Conky") \
+   "ConkySM") \
 /bin/sh $HOME/baconTelegraph/conkySM.sh
 ;;
-   "JS8call From SRC") \
+   "JS8Call") \
 /bin/sh $HOME/baconTelegraph/js8call_src.sh
 ;;
    *) \
