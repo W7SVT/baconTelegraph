e678a36f4096ae2d65df202adab9fdef5fa1b9a1
W7SVT <w7svt@icloud.com>
2023-10-19 07:39:27 -0700

    updated script with changes from RPi4

--- fldigi_src.sh
+++ fldigi_src.sh
@@ -18,15 +18,40 @@ echo "# Install dependencies      #"
 echo "#############################"
 
 sudo apt-get update
+
+# Check if the system is running a 64-bit or 32-bit OS
+if [ "$(uname -m)" == "x86_64" ]; then
+    libjpegVER=libjpeg62-turbo-dev
+elif [ "$(uname -m)" == "i686" ] || [ "$(uname -m)" == "i386" ]; then
+    libjpegVER=libjpeg9-dev
+else
+    echo "Unable to determine the OS architecture."
+fi
+
+# Check if the system is Raspberry Pi 4
+if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
+    optim=rpi4
+else
+    optim=native
+fi
+
 sudo apt-get install -y \
+  libfftw3-dev \
   libfltk1.3-dev \
-  libportaudio2 \
+  $libjpegVER \
+  libjpeg-dev \
+  libltdl-dev \
   libpulse-dev \
   libsamplerate0-dev \
-  libfftw3-dev \
-  libltdl-dev \
-  libjpeg-dev \
-  libsndfile1-dev
+  libsndfile1-dev \
+  libudev-dev \
+  libxcursor-dev \
+  libxft-dev \
+  libxinerama-dev \
+  portaudio19-dev \
+  texinfo
+
+sudo usermod -a -G dialout pi
 
 echo "#############################" 
 echo "# Set variables for Fldigi  #"
@@ -42,23 +67,38 @@ fldigi_ver=$(wget -qO- $fldigi_url | \
   tail -n 1 | \
   sed -E 's/fldigi-([0-9]+\.[0-9]+\.[0-9]+)\.tar\.gz/\1/')
 
+flrig_url="http://www.w1hkj.com/files/flrig/"
+
+flrig_ver=$(wget -qO- $flrig_url | \
+  grep -o 'href="flrig-[0-9]\+\.[0-9]\+\.[0-9]\+\.tar\.gz"' | \
+  cut -d'"' -f2 | \
+  tail -n 1 | \
+  sed -E 's/flrig-([0-9]+\.[0-9]+\.[0-9]+)\.tar\.gz/\1/')
+
 echo "#############################" 
-echo "# Downloading Fldigi        #"
+echo "# Downloading Fldigi FLrig  #"
 echo "#############################"
 
 cd $HOME
-
 wget $fldigi_url"fldigi-"$fldigi_ver.tar.gz -O - | tar -xz
-
-cd "fldigi-"$fldigi_ver
+wget $flrig_url"flrig-"$flrig_ver.tar.gz -O - | tar -xz
 
 echo "#######################" 
 echo "# Installing Fldigi   #"
 echo "#######################"
+cd $HOME
+cd "fldigi-"$fldigi_ver
+./configure --prefix=/usr/local --enable-static --enable-optimizations=$optim
+make .. -j$(nproc)
+sudo make install
+sudo make clean
 
-./configure --prefix=/usr/local --enable-static
-mkdir build
-cd build
+echo "#######################" 
+echo "# Installing Flrig    #"
+echo "#######################"
+cd $HOME
+cd "flrig-"$flrig_ver
+./configure --prefix=/usr/local --enable-static --enable-optimizations=$optim
 make .. -j$(nproc)
 sudo make install
 sudo make clean
